48834403b2a1b516f627d27e33b10726c7abd1bd
Yuya Kono <takohati0821@gmail.com>
2021-07-15 17:10:38 +0900

    Add `ident`, `nat`, `space`, `token`

--- src/native.c
+++ src/native.c
@@ -1,3 +1,4 @@
+#include <gc.h>
 #include <string.h>
 #include <stdint.h>
 
@@ -7,4 +8,20 @@ int32_t is_digit(char c) {
 
 int32_t is_lower(char c) {
   return 'a' <= c && c <= 'z';
+}
+
+int32_t is_upper(char c) {
+  return 'A' <= c && c <= 'Z';
+}
+
+int32_t is_alphanum(char c) {
+  return is_digit(c) || is_lower(c) || is_upper(c);
+}
+
+char* string_cons(char c, char* str) {
+  char *new = GC_MALLOC(sizeof(char) + 1 + strlen(str) + 1);
+  new[0] = c;
+  new[1] = '\0';
+  strcat(new, str);
+  return new;
 }
